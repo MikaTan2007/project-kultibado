b3e4e5ddbcf97eb84b7d87d8fa7833e6b9759710
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-08-18 22:56:49 -0700

    Added logo image

--- kultibado/app/page.tsx
+++ kultibado/app/page.tsx
@@ -6,6 +6,14 @@ export default function LoginPage() {
   return (
     <div>
       <div className="flex flex-col items-center pt-10">
+        <Image 
+          className = "w-auto h-auto"
+          src = "/images/kultibado_text.png" 
+          width = {400}
+          height = {400}
+          alt = "logo"
+          priority={true}
+        />
       </div>
       <div className = "pt-10">
         <LoginForm></LoginForm>
